bad5470a7a195c55d2d7855ac99b59d0af6d68bf
thecomplexjason <122879356+thecomplexjason@users.noreply.github.com>
2025-09-20 19:20:01 +0300

    Delete testing.lua

--- testing.lua
+++
@@ -1,6 +0,0 @@
-game.StarterGui:SetCore("SendNotification", {
-    Title = "AZC HUB | 99 Nights in the Forest",
-    Text = "تم ايقاف الاسكربت لتحديثه",
-    Duration = 999,
-    Button1 = "Script By FR3ON",
-})
